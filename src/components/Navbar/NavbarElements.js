f943e602a5f1e4987a61644cddf2b6ec5c85f97d
sohamdutta <xzoham@gmail.com>
2021-05-03 11:11:17 -0400

    expanding search bar added

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -81,3 +81,49 @@ cursor: pointer;
   border-bottom: 3px solid #01bf71;
 }
 `
+export const Form = styled.form`
+margin-top: 4px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: black;
+  /* Change width of the form depending if the bar is opened or not */
+  width: ${props => (props.barOpened ? "20rem" : "1rem")};
+  /* If bar opened, normal cursor on the whole form. If closed, show pointer on the whole form so user knows he can click to open it */
+  cursor: ${props => (props.barOpened ? "auto" : "pointer")};
+  padding: 2rem;
+  height: 2rem;
+  border-radius: 10rem;
+  transition: width 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+`;
+
+export const Input = styled.input`
+  font-size: 14px;
+  line-height: 1;
+  background-color: transparent;
+  width: 100%;
+  margin-left: ${props => (props.barOpened ? "1rem" : "0rem")};
+  border: none;
+  color: white;
+  transition: margin 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+  &::placeholder {
+    color: white;
+  }
+`;
+
+export const Button = styled.button`
+  pointer-events: ${props => (props.barOpened ? "auto" : "none")};
+  cursor: ${props => (props.barOpened ? "pointer" : "none")};
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: white;
+  font-size: 30px
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements'
+import React, { useState, useRef, useEffect } from "react";
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, Form, Button, Input } from './NavbarElements'
 import { FaBars } from 'react-icons/fa'
+import { BsSearch } from 'react-icons/bs'
 
 export default function Navbar() {
+  const [input, setInput] = useState("");
+  const [barOpened, setBarOpened] = useState(false);
+  const formRef = useRef();
+  const inputFocus = useRef();
+
+  const onFormSubmit = e => {
+    // When form submited, clear input, close the searchbar and do something with input
+    e.preventDefault();
+    setInput("");
+    setBarOpened(false);
+    // After form submit, do what you want with the input value
+    console.log(`Form was submited with input: ${input}`);
+  };
+
   return (
     <>
       <Nav>
@@ -15,7 +30,43 @@ export default function Navbar() {
           </MobileIcon>
           <NavMenu>
             <NavItem>
+              <Form
+                barOpened={barOpened}
+                onClick={() => {
+                  // When form clicked, set state of baropened to true and focus the input
+                  setBarOpened(true);
+                  inputFocus.current.focus();
+                }}
+                // on focus open search bar
+                onFocus={() => {
+                  setBarOpened(true);
+                  inputFocus.current.focus();
+                }}
+                // on blur close search bar
+                onBlur={() => {
+                  setBarOpened(false);
+                }}
+                // On submit, call the onFormSubmit function
+                onSubmit={onFormSubmit}
+                ref={formRef}
+              >
+                <Button type="submit" barOpened={barOpened}>
+                  <BsSearch />
+                </Button>
+                <Input
+                  onChange={e => setInput(e.target.value)}
+                  ref={inputFocus}
+                  value={input}
+                  barOpened={barOpened}
+                  placeholder="Search for a movie..."
+                />
+              </Form>
+            </NavItem>
+            <NavItem>
               <NavLinks to='home'>Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='nominations'>My Nominations</NavLinks>
             </NavItem>
           </NavMenu>
         </NavbarContainer>
