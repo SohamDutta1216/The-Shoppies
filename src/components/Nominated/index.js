2edfc9223ff4971f5f5ce4ec5291c725ed9fa68d
sohamdutta <xzoham@gmail.com>
2021-05-07 06:15:19 -0400

    tooltip added to remove movie

--- src/components/Nominated/index.js
+++ src/components/Nominated/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { IoMdRemoveCircleOutline } from "react-icons/io";
-
+import ReactTooltip from 'react-tooltip';
 
 const index = ({ nominate, removeNomination }) => {
   const movie = nominate[0]
@@ -13,10 +13,11 @@ const index = ({ nominate, removeNomination }) => {
         <div className='image-container d-flex justify-content-start m-3'>
           <img src={movie.Poster} alt='Movie' style={{ width: '200px', height: '300px' }} />
           <div className="overlay d-flex align-items-center justify-content-center">
+            <ReactTooltip />
             <div className='col'>
               <p className='bio'>{movie.Title}</p>
               <p className='bio'>{movie.Year}</p>
-              <IoMdRemoveCircleOutline onClick={click} />
+              <IoMdRemoveCircleOutline data-tip='Remove' onClick={click} />
             </div>
           </div>
         </div>
