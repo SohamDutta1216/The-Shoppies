e3487f1ed5d0a2680843db4ee1445d205f03ab9e
sohamdutta <xzoham@gmail.com>
2021-05-08 21:07:49 -0400

    modal added

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './movie.css'
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import ReactTooltip from 'react-tooltip';
 import FetchInfo from '../../hooks/fetchInfo'
+import Modal from "react-modal";
+import { CgClose } from 'react-icons/cg';
 
-const index = ({ movie, nominations, setNominations }) => {
+export default function Index({ movie, nominations, setNominations }) {
+  const [showModal, setShowModal] = useState(false);
   const { idSearch, setId, info } = FetchInfo()
 
   function click() {
     setNominations({ ...nominations, [movie.imdbID]: [movie] })
   }
-
+  function triggerModal() {
+    setShowModal(prev => !prev)
+  }
   function modal(e) {
     setId(e.imdbID)
+    triggerModal()
     console.log(info)
   }
   return (
@@ -28,6 +34,33 @@ const index = ({ movie, nominations, setNominations }) => {
               <p className='bio'>{movie.Year}</p>
             </div>
             <AiOutlineStar data-tip='Nominate this movie' onClick={click} />
+
+            <Modal
+              isOpen={showModal}
+              onRequestClose={triggerModal}
+              contentLabel="My dialog"
+              className="mymodal"
+              overlayClassName="myoverlay"
+              closeTimeoutMS={500}
+            >
+
+              <CgClose style={{ height: '50px', width: '30px' }} className='float-right' onClick={triggerModal} />
+              <div className='contain'>
+                <h1 >{info.Title}</h1>
+                <img src={info.Poster} alt='Movie' style={{ width: '200px', height: '300px', }} />
+
+                <div style={{}}>imdb rating: {info.imdbRating}</div>
+                <div  >Runtime: {info.Runtime}</div>
+                <div >Genre: {info.Genre}</div>
+                <div >Rated {info.Rated}</div>
+                <div >Released {info.Released}</div>
+              </div>
+              <div >{info.Plot}</div>
+              <div >Cast: {info.Actors}</div>
+
+
+            </Modal>
+
           </div>
         </div>
       </div>
@@ -35,4 +68,4 @@ const index = ({ movie, nominations, setNominations }) => {
   )
 }
 
-export default index
+
